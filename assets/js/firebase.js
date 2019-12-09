740139a1b6522d6a01e23868094f3cf6aec770a4
LunarTiger <lunartiger@live.com>
2019-12-09 05:19:21 -0500

    :no_entry_sign: :tada:

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,4 +1,3 @@
-/*
 var config = {
 	databaseURL: "https://vr-productions-fce36.firebaseio.com/",
 };
@@ -33,43 +32,6 @@ master.on('value', (function(snapshot) {
 					document.getElementById('omnembed').src = "https://www.youtube.com/embed/"+omnVal;
 				}));
 			}
-		}));
-	}
-}));
-*/
-
-var Rooftop = {
-	databaseURL: "https://vr-productions-fce36.firebaseio.com/the-rooftop",
-};
-firebase.initializeApp(Rooftop);
-var rooftopDatabase = firebase.database();
-var rooftop = rooftopDatabase.ref('id');
-var rooftopstate = rooftopDatabase.ref('state')
-rooftopstate.on('value', (function(snapshot) { 
-	var rooftopstateVal = snapshot.val();
-	if(rooftopstateVal){
-		rooftop.on('value', (function(snapshot) {
-			var rooftopVal = snapshot.val();
-			document.getElementById('rooftopembed').style = "max-width:100%; height:480px; width:855px; border:0px";
-			document.getElementById('rooftopembed').src = "https://www.youtube.com/embed/"+rooftopVal;
-		}));
-	}
-}));
-
-var open_mic_night = {
-	databaseURL: "https://vr-productions-fce36.firebaseio.com/open-mic-night",
-};
-firebase.initializeApp(open_mic_night);
-var omnDatabase = firebase.database();
-var omn = omndatabase.ref('id');
-var omnstate = omndatabase.ref('state')
-omnstate.on('value', (function(snapshot) { 
-	var omnstateVal = snapshot.val();
-	if(omnstateVal){
-		omn.on('value', (function(snapshot) {
-			var omnVal = snapshot.val();
-			document.getElementById('omnembed').style = "max-width:100%; height:480px; width:855px; border:0px";
-			document.getElementById('omnembed').src = "https://www.youtube.com/embed/"+omnVal;
 		}));
 	}
 }));
