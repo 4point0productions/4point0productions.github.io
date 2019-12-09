e93701ae7e7576a044b5f83b3036431619cae9e7
LunarTiger <lunartiger@live.com>
2019-12-08 22:14:39 -0500

    updated firebase

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -5,9 +5,18 @@ firebase.initializeApp(config);
 var database = firebase.database();
 var state = database.ref('state');
 state.on('value', (function(snapshot) {
-	var id = database.ref('id');
-	id.on('value', (function(snapshot) {
-		var idVal = snapshot.val();
-		document.getElementById('embed').src = "https://www.youtube.com/embed/"+idVal;
+	//Rooftop Video
+	var rooftop = database.ref('rooftop');
+	rooftop.on('value', (function(snapshot) {
+		var rooftopVal = snapshot.val();
+		document.getElementById('rooftopembed').src = "https://www.youtube.com/embed/"+rooftopVal;
 	}));
+	//Open Mic Night Video
+	/*
+	var omn = database.ref('rooftop');
+	omn.on('value', (function(snapshot) {
+		var omnVal = snapshot.val();
+		document.getElementById('omnembed').src = "https://www.youtube.com/embed/"+omnVal;
+	}));
+	*/
 }));
